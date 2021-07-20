91be8f4b50f2cb182fa19550346e06865ea6335f
Diego Solis <disc3110@gmail.com>
2021-07-20 12:21:46 -0500

    add Local Storage test

--- src/scripts/components/task/__tests__/task-functions.test.js
+++ src/scripts/components/task/__tests__/task-functions.test.js
@@ -3,28 +3,6 @@
  */
 import Task from '../task.js';
 import taskList from '../../list/task-list.js';
-import LocalStorage from '../../utils/localStorage.js';
-
-jest.mock('../../utils/localStorage.js');
-
-describe('Local storage mock operations', () => {
-  beforeAll(() => {
-    LocalStorage.mockImplementation(() => ({
-      localData: [],
-      loadInputData() {
-        return this.localData;
-      },
-      saveData(data) {
-        this.localData.push(data);
-      },
-    }));
-  });
-  it('loadInputData length should be greater than 1', () => {
-    const localStorage = new LocalStorage();
-    localStorage.saveData('object 1');
-    expect(localStorage.loadInputData().length).toBe(1);
-  });
-});
 
 describe('Task creation', () => {
   test('Create a new task', () => {
